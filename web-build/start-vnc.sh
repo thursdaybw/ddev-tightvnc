9086fa89cde6c8b45d97f4c19f75962283958c6c
Bevan Wishart <bevan.wishart@gmail.com>
2024-06-23 18:02:28 +1000

    Remove requirment for xfce

--- web-build/start-vnc.sh
+++ web-build/start-vnc.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
-echo "Starting VNC server setup..."
+
+#ddev-generated
 
 VNC_DIR="${HOME}/.vnc"
+
+export PASSWORD=${VNC_PASSWORD:-password}
+export DISPLAY=:1
+
 mkdir -p ${VNC_DIR}
-echo "Created .vnc directory"
-
-echo "password" | vncpasswd -f > ${VNC_DIR}/passwd
-echo "Set VNC password"
-
+echo $PASSWORD | vncpasswd -f > ${VNC_DIR}/passwd
 chmod 600 ${VNC_DIR}/passwd
-echo "Set password permissions"
 
-echo -e "#!/bin/bash\nstartxfce4 &" > ${VNC_DIR}/xstartup
-echo "Created xstartup script"
+echo -e "#!/bin/bash\n" > ${HOME}/.xsession
 
-chmod +x ${VNC_DIR}/xstartup
-echo "Set xstartup script permissions"
+vncserver $DISPLAY -geometry 1280x800 -depth 24
 
-vncserver :1 -geometry 1280x800 -depth 24
-echo "Started VNC server"
-
-tail -f ${VNC_DIR}/*.log
+tail -f ${VNC_DIR}/.vnc/*.log
 
